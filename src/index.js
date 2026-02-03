6e0b35726bab41189fbf0f2cd25ee8e66c3d9eae
lizzaaabb <lbogveradze12@gmail.com>
2026-02-03 15:38:09 +0400

    again

--- src/index.js
+++ src/index.js
@@ -2,23 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-// Check if the app was pre-rendered by react-snap
-if (rootElement.hasChildNodes()) {
-  // If pre-rendered, use hydrateRoot instead of createRoot
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  // Normal render for development
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
